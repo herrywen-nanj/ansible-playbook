eebc5f7e23a1c77f282a723cf7fb9d3ddc767562
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2020-08-21 12:10:55 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+# 变量获取
+git_api_address=`grep "git_api_address" hosts_$2 | awk -F "=" '{print $2}'`
+git_pc_address=`grep "git_pc_address" hosts_$2 | awk -F "=" '{print $2}'`
+git_admin_address=`grep "git_admin_address" hosts_$2 | awk -F "=" '{print $2}'`
+git_h5_address=`grep "git_h5_address" hosts_$2 | awk -F "=" '{print $2}'`
+path=$(dirname "`readlink -f $0`")
 case $1 in
    api)
-         ./api_build.sh $2 $3
+         /bin/bash $path/api_build.sh $2 $3
          ;;
    pc)
-         ./pc_build.sh  $2 $3
-         ;; 
-   admin)      
-         ./admin_build.sh $2 $3
-         ;; 
-   h5)
-         ./h5_build.sh  $2 $3 
+         /bin/bash $path/pc_build.sh  $2 $3
          ;;
-   all)
-         ./api_build.sh $2 $3 && ./pc_build.sh  $4 $3 && ./admin_build.sh $5 $3 && ./h5_build.sh $6 $3
+   admin)
+         /bin/bash $path/admin_build.sh $2 $3
+         ;;
+   h5)
+         /bin/bash $path/h5_build.sh  $2 $3
+         ;;
+   deploy)
+         /bin/bash $path/api_build.sh $git_api_address $2 && /bin/bash $path/pc_build.sh  $git_pc_address $2 && /bin/bash $path/admin_build.sh $git_admin_address $2 && /bin/bash $path/h5_build.sh $git_h5_address $2
          ;;
 esac
+
